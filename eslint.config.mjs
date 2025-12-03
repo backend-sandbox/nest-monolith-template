423b8eeb37221db5c90cf72eac69825439e7beca
Wastardy <wastardy.k@gmail.com>
2025-12-03 22:50:30 +0200

    feat(configs): update prettier and eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,32 +4,32 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   {
-    ignores: ['eslint.config.mjs'],
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.mjs', '.ts'],
+        },
+      },
+    },
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+  { languageOptions: { globals: globals.browser } },
   {
     rules: {
+      'no-case-declarations': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off', // enable in the future
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      'import/no-cycle': 'error',
     },
   },
-);
+];
