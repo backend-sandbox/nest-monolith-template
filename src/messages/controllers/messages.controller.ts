293d4b469460c1391b7bc873c508f1d36e170033
Wastardy <wastardy.k@gmail.com>
2025-12-05 12:10:47 +0200

    feat(message-module): add message module to test how to work with typeorm

--- src/messages/controllers/messages.controller.ts
+++ src/messages/controllers/messages.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateMessageDto } from '../dtos';
+import { Message } from '../entities';
+import { MessagesService } from '../services';
 
 @ApiTags('Messages API')
 @Controller('messages')
 export class MessagesController {
+  constructor(private readonly messagesService: MessagesService) {}
+
   @ApiOperation({ summary: 'Get all messages' })
+  @ApiResponse({ status: 200, description: 'List of messages', type: [Message] })
   @Get('messages')
-  async getMessages() {}
+  async getAllMessages(): Promise<Message[]> {
+    return await this.messagesService.getAllMessages();
+  }
 
   @ApiOperation({ summary: 'Get message by id' })
+  @ApiResponse({ status: 200, description: 'Message found', type: Message })
   @Get('messages/:messageId')
-  async getMessageById() {}
+  async getMessageById(@Param('messageId') messageId: string): Promise<Message> {
+    return await this.messagesService.getMessageById(messageId);
+  }
 
   @ApiOperation({ summary: 'Create a new message' })
+  @ApiResponse({ status: 201, description: 'Message created', type: Message })
   @Post('messages')
-  async createMessage() {}
+  async createMessage(@Body() body: CreateMessageDto): Promise<Message> {
+    return await this.messagesService.createMessage(body);
+  }
 }
---
+++ src/messages/entities/index.ts
@@ -0,0 +1,2 @@
+export { Message } from './message.entity';
+export { User } from './user.entity';
