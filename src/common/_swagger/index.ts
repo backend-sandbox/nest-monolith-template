8b6e47296d42eae3bfb8fab6db6d61add24b5ab0
Wastardy <wastardy.k@gmail.com>
2025-12-21 07:39:38 +0200

    feat(swagger): move swagger to common utils

---
+++ src/common/_swagger/index.ts
@@ -0,0 +1,1 @@
+export { buildSwagger } from './swagger.util';
--- src/common/_swagger/swagger.util.ts
+++ src/common/_swagger/swagger.util.ts
@@ -1,39 +1,43 @@
+import { TSwaggerOptions } from '@common/types';
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as fs from 'fs';
 
 type SwaggerOperation = { get: (key: string) => string };
 
-export const buildSwagger = (args: {
-  app: INestApplication;
-  service: string;
-}) => {
+export const buildSwagger = (args: { app: INestApplication; options: TSwaggerOptions }) => {
+  const { app, options } = args;
+
   try {
     const config = new DocumentBuilder()
-      .setTitle(`My NestJS Sandbox ${args.service.toUpperCase()}`)
-      .setVersion('1.0')
+      .setTitle(`${options.title ?? 'NestJS Monolith Template API'} ${options.code.toUpperCase()}`)
+      .setVersion(options.version ?? '1.0')
+      .setDescription(options.description ?? 'The NestJS Monolith Template API description')
       .addBearerAuth()
       .build();
 
-    const document = SwaggerModule.createDocument(args.app, config, {
+    const document = SwaggerModule.createDocument(app, config, {
       operationIdFactory: (controllerKey: string, methodKey: string) => {
         return `${controllerKey.replace('Controller', '')}_${methodKey}`;
       },
     });
 
     if (process.env.NODE_ENV !== 'production') {
-      SwaggerModule.setup('swagger', args.app, document, {
+      SwaggerModule.setup('swagger', app, document, {
         jsonDocumentUrl: 'swagger.json',
+        customCssUrl: '/public/_swagger/swagger.theme.css',
+        customJs: '/public/_swagger/swagger.theme.js',
+
         swaggerOptions: {
           persistAuthorization: true,
 
           operationsSorter: (a: SwaggerOperation, b: SwaggerOperation) => {
             const order = ['get', 'post', 'put', 'patch', 'delete'];
 
-            return (
-              order.indexOf(a.get('method')) - order.indexOf(b.get('method'))
-            );
+            return order.indexOf(a.get('method')) - order.indexOf(b.get('method'));
           },
+
+          tagsSorter: 'alpha',
         },
       });
     }
@@ -42,10 +46,7 @@ export const buildSwagger = (args: {
     if (!fs.existsSync(swaggerFolder)) {
       fs.mkdirSync(swaggerFolder, { recursive: true });
     }
-    fs.writeFileSync(
-      swaggerFolder + `/${args.service.toLowerCase()}.swagger.json`,
-      JSON.stringify(document),
-    );
+    fs.writeFileSync(swaggerFolder + `/${options.code.toLowerCase()}.swagger.json`, JSON.stringify(document));
   } catch (ex) {
     console.error(ex, 'Error on swagger setup');
   }
